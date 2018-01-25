be93e5b52d946cc7521b5b0247843d9d0d6cb3e5
bg-wa <bill.j.grow@gmail.com>
2018-01-25 13:00:18 -0800

    Init

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+qr_max_size = 4296;
+
+function generateQR(){
+  $('#status').text('Processing... this will take a while...');
+  setTimeout(function (){
+    var files = document.getElementById("uploadInput").files;
+    $.each(files, function(index, file){
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = function () {
+        var base64 = reader.result;
+        var qr_chunks = base64.match(/.{1,100}/g);
+        var chunks_length = qr_chunks.length;
+        $('#qr_chunks').text('/'+ chunks_length);
+        $('#qr_codes').text('');
+        setTimeout(function(){
+          $.each(qr_chunks, function(i, chunk){
+            $('#current_qr_chunk').text(i + 1);
+            setTimeout(function(){
+              var qr_obj_id = 'qrcode_' + i;
+              $('#qr_codes').append('<div id="' + qr_obj_id + '" style="margin:10px; max-width: 64px; display: inline-block">'+i+'</div>');
+              $('#' + qr_obj_id).qrcode({render: 'div', size: 100, text: chunk});
+              if((i + 1) == chunks_length ){
+                $('#status').text('Finished!');
+              }
+            }, 1);
+          });
+        },1);
+      };
+      reader.onerror = function (error) {
+        console.log('Error: ', error);
+      };
+    });
+  }, 1);
+
+}
