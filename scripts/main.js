abff881eb174a8720a68ddbd41490c5232c09b67
bg-wa <bill.j.grow@gmail.com>
2018-01-25 14:37:47 -0800

    Render with Canvas... SOOO much faster!

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
-qr_max_size = 4296;
-
 function generateQR(){
   $('#status').text('Processing... this will take a while...');
   setTimeout(function (){
@@ -9,6 +7,7 @@ function generateQR(){
       reader.readAsDataURL(file);
       reader.onload = function () {
         var base64 = reader.result;
+        // QR String Size: 100 Char
         var qr_chunks = base64.match(/.{1,100}/g);
         var chunks_length = qr_chunks.length;
         $('#qr_chunks').text('/'+ chunks_length);
@@ -18,8 +17,8 @@ function generateQR(){
             setTimeout(function(){
               $('#current_qr_chunk').text(i + 1);
               var qr_obj_id = 'qrcode_' + i;
-              $('#qr_codes').append('<div id="' + qr_obj_id + '" style="margin:10px; max-width: 64px; display: inline-block">'+i+'</div>');
-              $('#' + qr_obj_id).qrcode({render: 'div', size: 100, text: chunk});
+              $('#qr_codes').append('<div id="' + qr_obj_id + '" style="margin:10px; max-width: 100px; display: inline-block">'+i+'</div>');
+              $('#' + qr_obj_id).qrcode({render: 'canvas', size: 100, text: chunk});
               if((i + 1) == chunks_length ){
                 $('#status').text('Finished!');
               }
