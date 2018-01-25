b77d03f9db81326e962b9124e4e3f4fef82cd717
bg-wa <bill.j.grow@gmail.com>
2018-01-25 15:01:41 -0800

    Dynamic QR size

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # QR Extractor
-1. Open the index.html page in any broswer.
+1. Open the index.html page in any browser.
 2. Select the file you want to process.
 3. Wait...  The code will spit out QRs as they are processed.  It can run in the background, then you can take a picture of them all at once with your phone when its finished. 
+
+Big Buck Bunny (5.5mb) takes about 6:30 minutes.
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+qr_string_size = 500;
+qr_image_size = 200;
+
 function generateQR(){
   $('#status').text('Processing... this will take a while...');
   setTimeout(function (){
@@ -7,8 +10,8 @@ function generateQR(){
       reader.readAsDataURL(file);
       reader.onload = function () {
         var base64 = reader.result;
-        // QR String Size: 100 Char
-        var qr_chunks = base64.match(/.{1,100}/g);
+        var strRegExPattern = '.{1,'+ qr_string_size +'}';
+        var qr_chunks = base64.match(new RegExp(strRegExPattern,'g'));
         var chunks_length = qr_chunks.length;
         $('#qr_chunks').text('/'+ chunks_length);
         $('#qr_codes').text('');
@@ -17,9 +20,9 @@ function generateQR(){
             setTimeout(function(){
               $('#current_qr_chunk').text(i + 1);
               var qr_obj_id = 'qrcode_' + i;
-              $('#qr_codes').append('<div id="' + qr_obj_id + '" style="margin:10px; max-width: 100px; display: inline-block">'+i+'</div>');
-              $('#' + qr_obj_id).qrcode({render: 'canvas', size: 100, text: chunk});
-              if((i + 1) == chunks_length ){
+              $('#qr_codes').append('<div id="' + qr_obj_id + '" style="margin:10px; max-width: ' + qr_image_size + 'px; display: inline-block">'+i+'</div>');
+              $('#' + qr_obj_id).qrcode({render: 'canvas', size: qr_image_size, text: chunk});
+              if((i + 1) === chunks_length ){
                 $('#status').text('Finished!');
               }
             }, 1);
