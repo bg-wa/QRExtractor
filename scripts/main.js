cfb6e3db01ff2fcd1573a912a2dae70fd58d7f9e
bg-wa <bill.j.grow@gmail.com>
2018-01-25 21:23:10 -0800

    Ditched jQuery

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-var qr_string_size = 500;
+var qr_string_size = 100;
 var qr_image_size = 200;
 var playback_delay = 200;
 
@@ -7,63 +7,84 @@ var playback_delay = 200;
 // These Chunks are then converted into QR Codes and displayed in the browser.
 // Timeouts are to prevent blocking in the browser
 
-function generateQR(){
-  $('#status').text('Processing...');
-  setTimeout(function (){
-    var files = document.getElementById("uploadInput").files;
-    $.each(files, function(index, file){
-      var reader = new FileReader();
-      reader.readAsDataURL(file);
-      // Convert to base64
-      reader.onload = function () {
-        var base64 = reader.result;
-        var strRegExPattern = '.{1,'+ qr_string_size +'}';
-        // Chunk that string
-        var qr_chunks = base64.match(new RegExp(strRegExPattern,'g'));
-        var chunks_length = qr_chunks.length;
-        $('#qr_chunks').text('/'+ chunks_length);
-        $('#qr_codes').text('');
-        setTimeout(function(){
-          $.each(qr_chunks, function(i, chunk){
-            setTimeout(function(){
-              // Create QR Code for Chunk
-              $('#current_qr_chunk').text(i + 1);
-              var qr_obj_id = 'qrcode_' + i;
-              $('#qr_codes').append('<div id="' + qr_obj_id + '" style="margin:10px; max-width: ' + qr_image_size + 'px; display: inline-block">'+i+'</div>');
-              $('#' + qr_obj_id).qrcode({render: 'canvas', size: qr_image_size, text: chunk});
-              if((i + 1) === chunks_length ){
-                $('#status').html('Finished! <br/>' +
-                  '<a href="#" onclick="playback()">Playback All</a> <br/>' +
-                  '<a href="#" onclick="cancelPlayback = true;">Cancel Playback</a>');
-              }
-            }, 1);
-          });
-        },1);
-      };
-      reader.onerror = function (error) {
-        console.log('Error: ', error);
-      };
-    });
-  }, 1);
+var chunks_length;
+function generateQR() {
+    document.getElementById('status').innerHTML = 'Processing...';
+    setTimeout(function () {
+        var files = document.getElementById("uploadInput").files;
+        for (var index = 0; index < files.length; index++) {
+            var file = files[index];
+            var reader = new FileReader();
+            reader.readAsDataURL(file);
+            // Convert to base64
+            reader.onload = function () {
+                var base64 = reader.result;
+                var strRegExPattern = '.{1,' + qr_string_size + '}';
+                // Chunk that string
+                var qr_chunks = base64.match(new RegExp(strRegExPattern, 'g'));
+                chunks_length = qr_chunks.length;
+                document.getElementById('qr_chunks').innerHTML = '/' + chunks_length;
+                document.getElementById('qr_codes').innerHTML = '';
+                setTimeout(function () {
+                    for (var i = 0; i < qr_chunks.length; i++) {
+                        var chunk = qr_chunks[i];
+                        renderQR(chunk, i)
+                    }
+                }, 1);
+            };
+            reader.onerror = function (error) {
+                console.log('Error: ', error);
+            };
+        }
+    }, 1);
+}
+
+var css_string = 'margin:10px; max-width: ' + qr_image_size + 'px; display: inline-block';
+function renderQR(chunk, i) {
+    setTimeout(function () {
+        // Create QR Code for Chunk
+        document.getElementById('current_qr_chunk').innerHTML = i + 1;
+        var qr_obj_id = 'qrcode_' + i;
+        var placeholder = document.createElement('div');
+        placeholder.id = qr_obj_id;
+        placeholder.style.cssText = css_string;
+        placeholder.innerHTML = i;
+        document.getElementById('qr_codes').appendChild(placeholder);
+        new QRCode(document.getElementById(qr_obj_id), {
+            width: qr_image_size,
+            height: qr_image_size,
+            text: chunk,
+            correctLevel : QRCode.CorrectLevel.H
+        });
+        if ((i + 1) === chunks_length) {
+            document.getElementById('status').innerHTML = 'Finished! <br/>' +
+                '<a href="#" onclick="playback()">Playback All</a> <br/>' +
+                '<a href="#" onclick="cancelPlayback = true;">Cancel Playback</a>';
+        }
+    }, 1);
 }
 
 var cancelPlayback = false;
-function playback(){
-  cancelPlayback = false;
-  var all_qrs = $("[id^='qrcode_']");
-  var qr_array_size = $(all_qrs).length;
-  var current_qr = 0;
-  setInterval(function(){
-    if(!cancelPlayback){
-      $(all_qrs).hide();
-      $('#qrcode_'+ current_qr).show();
-      if((current_qr) === qr_array_size){
-        cancelPlayback = true;
-      }
-      current_qr ++;
-    }else{
-      $("[id^='qrcode_']").show();
-      current_qr = 0;
-    }
-  }, playback_delay);
+function playback() {
+    cancelPlayback = false;
+    var current_qr = 0;
+    setInterval(function () {
+        var all_qrs = document.querySelectorAll('[id^=qrcode_]');
+        if (!cancelPlayback) {
+            for (var index = 0; index < all_qrs.length; index++) {
+                var qr = all_qrs[index];
+                qr.style.cssText = 'display:none'
+            }
+            document.getElementById('qrcode_' + current_qr).style.cssText = css_string;
+            current_qr++;
+            if ((current_qr) === all_qrs.length) {
+                cancelPlayback = true;
+            }
+        } else {
+            for (var index = 0; index < all_qrs.length; index++) {
+                all_qrs[index].style.cssText = css_string;
+            }
+            current_qr = 0;
+        }
+    }, playback_delay);
 }
